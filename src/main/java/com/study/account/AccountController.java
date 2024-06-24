68e1b6a6ca2639c65e2ed4c12e2c41ebdccc7dd5
rudqls007 <rudqls007@gmail.com>
2024-06-24 23:51:39 +0900

    회원가입 뷰

--- src/main/java/com/study/account/AccountController.java
+++ src/main/java/com/study/account/AccountController.java
@@ -2,7 +2,11 @@ package com.study.account;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class AccountController {
@@ -14,4 +18,17 @@ public class AccountController {
 
         return "account/sign-up";
     }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpFormDto signUpFormDto, Errors errors) {
+
+        if(errors.hasErrors()){
+            return "account/sign-up";
+    }
+
+
+        //TODO 회원 가입 처리
+        return "redirect:/";
+
+
 }
