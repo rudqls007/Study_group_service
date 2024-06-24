33752dfb86d80c39eed04ede48393ac10b30e743
rudqls007 <rudqls007@gmail.com>
2024-06-24 22:51:59 +0900

    회원가입 뷰

---
+++ src/main/java/com/study/account/SignUpFormDto.java
@@ -0,0 +1,13 @@
+package com.study.account;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class SignUpFormDto {
+
+
+    private String nickname;
+    private String email;
+    private String password;
+}
